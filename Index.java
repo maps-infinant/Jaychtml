074eafc8319c9dfbccebe6118ebd924b1d29c6d1
Elicit <ParkourElicit@gmail.com>
2019-10-11 16:16:17 -0500

    Started the index I guess IDK
    Doesn't really change much but added some file that I found.

---
+++ Index.java
@@ -0,0 +1,28 @@
+package main.Archer.loopy;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Index extends JFrame {
+	
+	public Index() {
+		//Variable declaration and crap
+		
+		
+		//Functional shit i guess
+		
+		
+		//and this is like the JFrame declaration magic. IDFK, I'm just here for the biscuits.
+		this.setPreferredSize(new Dimension(640, 480));
+		this.setMinimumSize(new Dimension(640, 480));
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		new Index();
+	}
+	
+}

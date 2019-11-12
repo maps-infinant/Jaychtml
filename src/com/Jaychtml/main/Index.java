1b1e319fedf80a71ae90607dde368c1f03013f0e
Elicit <ParkourElicit@gmail.com>
2019-11-12 15:03:59 -0600

    Created layout manually
    Added setbounds

--- src/com/Jaychtml/main/Index.java
+++ src/com/Jaychtml/main/Index.java
@@ -1,16 +1,17 @@
 package com.Jaychtml.main;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.LayoutManager;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
+import javax.swing.border.EtchedBorder;
 
 public class Index extends JFrame {
 	
@@ -21,12 +22,10 @@ public class Index extends JFrame {
 		JPanel inspectPanel = new JPanel();
 		JPanel editorPanel = new JPanel();
 		JPanel menuPanel = new JPanel();
-		LayoutManager layout = new SpringLayout();
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		
 		//Functional shit i guess
 		menuPanel.add(new JLabel("Menu"));
-		menuPanel.setMinimumSize(new Dimension(screenSize.width,screenSize.height/10));
 		treePanel.add(new JLabel("Tree"));
 		editorPanel.add(new JLabel("Editor"));
 		inspectPanel.add(new JLabel("Inspect"));
@@ -34,12 +33,21 @@ public class Index extends JFrame {
 		//fullscreen BITCCHES
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		this.setUndecorated(true);
+		this.setLayout(null);//Get rid of layout to make setBounds work
 		
-		//layout positioning i think
-		layout.addLayoutComponent(BorderLayout.NORTH, menuPanel);
-		layout.addLayoutComponent(BorderLayout.WEST, treePanel);
-		layout.addLayoutComponent(BorderLayout.EAST, editorPanel);
-		layout.addLayoutComponent(BorderLayout.SOUTH, inspectPanel);
+		//sizing and positioning
+		menuPanel.setBounds(new Rectangle(0, 0, screenSize.width, screenSize.height/10));
+		//menuPanel.setBackground(Color.RED);
+		menuPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+		treePanel.setBounds(new Rectangle(0,screenSize.height/10,screenSize.width*3/10,screenSize.height*7/10));
+		//treePanel.setBackground(Color.GREEN);
+		treePanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+		editorPanel.setBounds(new Rectangle(screenSize.width*3/10, screenSize.height/10, screenSize.width*7/10, screenSize.height*7/10));
+		//editorPanel.setBackground(Color.BLUE);
+		editorPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+		inspectPanel.setBounds(new Rectangle(0,screenSize.height*8/10,screenSize.width,screenSize.height*2/10));
+		//inspectPanel.setBackground(Color.YELLOW);
+		inspectPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
 		
 		//add key listener
 		this.addKeyListener(new KeyListener() {
@@ -64,10 +72,8 @@ public class Index extends JFrame {
 		this.add(treePanel);
 		this.add(editorPanel);
 		this.add(inspectPanel);
-		
-		this.setLayout(layout);
+
 		this.setVisible(true);
-		
 	}
 	
 	public static void main(String[] args) {
